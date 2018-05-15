e3f44f8176d06a7ef043dac3229e64785809ff29
Damako4 <36765481+Damako4@users.noreply.github.com>
2018-05-15 10:25:32 -0400

    Create Clear Cookies.js

---
+++ tools/Clear Cookies.js
@@ -0,0 +1,3 @@
+//Clears the cookies of the current page
+
+javascript:(function(){C=document.cookie.split("; ");for(d="."+location.host;d;d=(""+d).substr(1).match(/\..*$/))for(sl=0;sl<2;++sl)for(p="/"+location.pathname;p;p=p.substring(0,p.lastIndexOf('/')))for(i in C)if(c=C[i]){document.cookie=c+"; domain="+d.slice(sl)+"; path="+p.slice(1)+"/"+"; expires="+new Date((new Date).getTime()-1e11).toGMTString()}})()
