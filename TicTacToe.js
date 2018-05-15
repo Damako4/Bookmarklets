482c3733331882e0f6a75a96962c517cf17fe0a5
Damako4 <36765481+Damako4@users.noreply.github.com>
2018-05-15 13:23:18 -0400

    Create TicTacToe.js
    A game of tic-tac-toe

---
+++ TicTacToe.js
@@ -0,0 +1,3 @@
+//Allows for multiple games of tic-tac-toe
+
+javascript:(function(){var js=document.body.appendChild(document.createElement("script"));js.onerror=function(){alert("Sorry, the script could not be loaded.")};js.src="https://rawgit.com/Krazete/bookmarklets/master/tic.js"})();
