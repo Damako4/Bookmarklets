0b7014d6272d24b32735a1626b6dc4dafeea9023
Damako4 <36765481+Damako4@users.noreply.github.com>
2018-05-15 14:02:43 -0400

    Create Lupire.js

---
+++ Games/Lupire.js
@@ -0,0 +1,3 @@
+//Pinball game
+
+javascript:(function(){var js=document.body.appendChild(document.createElement("script"));js.onerror=function(){alert("Sorry, the script could not be loaded.")};js.src="https://rawgit.com/Krazete/bookmarklets/master/lupire.js"})();
