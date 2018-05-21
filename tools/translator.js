5864e0de5fc581a970c48efc37e17b2b59697f97
C0DY314 <39312859+C0DY314@users.noreply.github.com>
2018-05-21 16:21:43 -0400

    Create translator.js

---
+++ tools/translator.js
@@ -0,0 +1,3 @@
+//show translates 11 diffrent laungaes into english
+
+javascript:(function() { var d,b,f,u,L,i,K,t, LL="zh Chinese-simplified,zt Chinese-traditional,nl Dutch,fr French,de German,el Greek,it Italian,ja Japanese,ko Korean,pt Portuguese,ru Russian,es Spanish".split(","); d=open().document; b=d.body; f=d.createElement("form"); b.appendChild(f); f.action="http://babelfish.altavista.com/babelfish/trurl_pagecontent"; u=d.createElement("input"); u.name="url"; u.style.width="100%"; u.value=location; f.appendChild(u); for (i=0; L=LL[i]; ++i) { K = L.split(" "); f.appendChild(d.createElement("br")); t=d.createElement("button"); t.name="lp"; t.value=K[0]+"_en"; t.innerHTML = "<b>" + K[1] + "</b> to English"; f.appendChild(t); } })()
