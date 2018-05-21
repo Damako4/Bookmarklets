c2bb574f99e218f3ad9b1ec4b14bebe033a97b36
C0DY314 <39312859+C0DY314@users.noreply.github.com>
2018-05-21 16:23:38 -0400

    Create remote.js

---
+++ tools/remote.js
@@ -0,0 +1,3 @@
+//show creates a tab with setting and commands 
+
+javascript:window.name = 'receiver'; window.open('http://www.squarefree.com/bookmarklets/remotePageLinks.html','','resizable=yes,width=223,height=337,left=508,top=122'); void 0
