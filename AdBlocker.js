7ce20f8ec93b5ea8b529273d5a52cecea35e7bba
Damako4 <36765481+Damako4@users.noreply.github.com>
2018-05-21 14:15:58 -0400

    Create AdBlocker.js

---
+++ AdBlocker.js
@@ -0,0 +1,3 @@
+//Basically an ad-blocker
+
+javascript:void(F=document.getElementsByTagName('iframe'));for(f=0;f<F.length;f++)void(F[f].src='about:');void(F=document.getElementsByTagName('img'));for(f=0;f<F.length;f++)if(F[f].width=='468')void(F[f].src='about:');
