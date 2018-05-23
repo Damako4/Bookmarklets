a151e27ce2e43c671d85b1096bb8a4be19a4e6c0
C0DY314 <39312859+C0DY314@users.noreply.github.com>
2018-05-23 08:02:27 -0400

    Update and rename Flashlight.js to spotlight.js

--- spotlight.js
+++ spotlight.js
@@ -1,3 +1,3 @@
-//Shows a flashlight on the page
+//Shows a spotlight on the page
 
 javascript:(function(){var js=document.body.appendChild(document.createElement("script"));js.onerror=function(){alert("Sorry, the script could not be loaded.")};js.src="https://rawgit.com/Krazete/bookmarklets/master/mouselight.js"})();
