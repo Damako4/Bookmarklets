f941680f296635faa0ae71244bc68c13cc5d8ffb
Damako4 <36765481+Damako4@users.noreply.github.com>
2018-05-25 10:03:21 -0400

    Create GoGaurdianBlocker.js

---
+++ GoGaurdianBlocker.js
@@ -0,0 +1,3 @@
+//Makes it so teachers cannot close your tabs. (Requires a prompt to close your tab)
+
+javascript:window.onbeforeunload = function () { return "Do your want to close"; }; alert("Tab Secured");
