5b556fc91b1d2da7814380723ed483b23c4fcce7
Siddhant Bhardwaj <30597698+codewithsid@users.noreply.github.com>
2018-06-06 11:45:36 -0400

    Update GoGaurdianBlocker.js

--- GoGaurdianBlocker.js
+++ GoGaurdianBlocker.js
@@ -1,3 +1,7 @@
 //Makes it so teachers cannot close your tabs. (Requires a prompt to close your tab)
 
 javascript:window.onbeforeunload = function () { return "Do your want to close"; }; alert("Tab Secured");
+if window = true, then alert("Tab Secure")
+
+Else, if window = false 
+  then alert("Closing Tab")
