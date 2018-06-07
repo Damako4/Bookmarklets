6398a8267e3cd8a72f3574ccebfbb7c1f8a796d9
Damako4 <36765481+Damako4@users.noreply.github.com>
2018-06-07 10:41:23 -0400

    Create NinjaImages.js

---
+++ NinjaImages.js
@@ -0,0 +1,3 @@
+//Turns All The Pictures Into Ninja
+
+javascript:(function(){Array.prototype.slice.call(document.querySelectorAll('img')).map(function(el){el.src = '//encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRewzQ0rakhrK_tXzBaOeFTZGM7kURCAPofIP77ToEoE-quiBIG';});}())
