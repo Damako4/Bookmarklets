84c7ffc21e1dc05c6094ad0ad4d9e52dd11605ee
AlmaDev <36765481+AlmaDev@users.noreply.github.com>
2018-10-12 14:30:14 -0400

    Create PageSpeed.js

---
+++ tools/PageSpeed.js
@@ -0,0 +1,3 @@
+// Find page speed
+
+javascript:(function(){var t=window.WFP=window.WFP||{},e="https:"===window.location.protocol%3F"https:":"http:";t.bookmarklet=3,t.Picker%26%26t.Picker.show(),t.attached||t.Picker||(function(t){var e=document.createElement("link");e.rel="stylesheet",e.type="text/css",e.href=t,document.head.appendChild(e)}(e+"//gavrilov.co.uk/wfp/WFP.css"),function(t){var e=document.createElement("script");e.type="text/javascript",e.src=t,document.head.appendChild(e)}(e+"//gavrilov.co.uk/wfp/WFP.full.min.js"),t.attached=!0)})();
