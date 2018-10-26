ae562fd37dd74384496424437315678b3da45353
AlmaDev <36765481+AlmaDev@users.noreply.github.com>
2018-10-26 14:11:22 -0400

    Create keyclick.js

---
+++ Toys/keyclick.js
@@ -0,0 +1,1 @@
+javascript:window.addEventListener("keydown", dealWithKeyboard, false); window.addEventListener("keypress", dealWithKeyboard, false); window.addEventListener("keyup", dealWithKeyboard, false); function dealWithKeyboard(e) { alert("Hello")  }
